51d7e2d47d5fa131dd70a69350663ad2c8a66868
Prakhar Saxena <46266609+prakharkrsaxena@users.noreply.github.com>
2022-07-01 15:41:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+
+$("button").click(function(){
+var randomNumber1=(Math.floor(Math.random()*6)+1);
+var address1="images/dice"+randomNumber1+".png";
+var randomNumber2=(Math.floor(Math.random()*6)+1);
+var address2="images/dice"+randomNumber2+".png";
+document.querySelectorAll("img")[0].setAttribute("src",address1);
+document.querySelectorAll("img")[1].setAttribute("src",address2);
+if(randomNumber1>randomNumber2)
+document.querySelector("h1").innerHTML="🚩Player 1 wins";
+else if(randomNumber1<randomNumber2)
+document.querySelector("h1").innerHTML="Player 2 wins🚩";
+else 
+document.querySelector("h1").innerHTML="Draw";
+});
+
